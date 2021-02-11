8c6c5a125e2c57f7225091e69ad03ac4091f3acb
Lucas Andrade Maciel <lucas.maciel@localiza.com>
2021-02-11 16:03:08 -0300

    Testes unitários VeiculosController

--- src/Teste.El.Backend.Tests/Controllers/VeiculosControllerTest.cs
+++ src/Teste.El.Backend.Tests/Controllers/VeiculosControllerTest.cs
@@ -20,218 +20,232 @@ namespace Teste.El.Backend.Tests.Controllers
             _mapperFixture = mapperFixture;
         }
 
-        //#region Usuario
+        #region Veiculo
 
         /// <summary>
         /// Dado que:
-        ///    seja solicitado o cadastro de um usuario
+        ///    seja solicitado o cadastro de um veiculo
         /// Eu preciso 
         ///    validar se os dados informados são válidos e se não existem no banco
         /// Para 
         ///    Armazenar as informações no banco
         /// </summary>
-        //[Fact]
-        //public async void CadastrarUsuario_Sucesso()
-        //{
-        //    //Prepare           
-        //    var ctx = CancellationToken.None;
-        //    var controller = CriarUsuarioController();
-
-        //    var input = new UsuarioModel
-        //    {
-        //        Login = "123465",
-        //        Senha = "123456"
-        //    };
-
-        //    //Act
-        //    var result = await controller.CadastrarUsuario(input, ctx);
-
-        //    //Assert
-        //    Assert.IsType<OkResult>(result);
-        //}
-
-        ///// <summary>
-        ///// Dado que:
-        /////    seja solicitado o cadastro de um usuario
-        ///// Eu preciso 
-        /////    validar se os dados informados são válidos e se não existem no banco
-        ///// Para 
-        /////    Informar o motivo do usuario não poder ser cadastrado 
-        ///// </summary>
-        //[Fact]
-        //public async void CadastrarUsuario_Erro()
-        //{
-        //    //Prepare           
-        //    var ctx = CancellationToken.None;
-        //    var controller = CriarUsuarioController(true, true, false);
-
-        //    var input = new UsuarioModel
-        //    {
-        //        Login = "123465",
-        //        Senha = "123456"
-        //    };
-
-        //    //Act
-        //    var result = await controller.CadastrarUsuario(input, ctx);
-
-        //    //Assert
-        //    Assert.IsType<UnprocessableEntityObjectResult>(result);
-        //}
-
-        //#endregion
-
-        //#region Cliente
-
-        ///// <summary>
-        ///// Dado que:
-        /////    seja solicitado o cadastro de um cliente
-        ///// Eu preciso 
-        /////    validar se os dados informados são válidos e se não existem no banco
-        ///// Para 
-        /////    Armazenar as informações no banco
-        ///// </summary>
-        //[Fact]
-        //public async void CadastrarCliente_Sucesso()
-        //{
-        //    //Prepare           
-        //    var ctx = CancellationToken.None;
-        //    var controller = CriarUsuarioController();
-
-        //    var endereco = new EnderecoFixture().CriarEndereco();
-
-        //    var input = new ClienteModel
-        //    {
-        //        Aniversario = DateTime.Now.AddYears(-20),
-        //        Cpf = "90459735020",
-        //        Nome = "Teste",
-        //        Endereco = new ClienteModel.DadosEndereco
-        //        {
-        //            Cep = endereco.Cep,
-        //            Cidade = endereco.Cidade,
-        //            Complemento = endereco.Complemento,
-        //            Estado = endereco.Estado,
-        //            Logradouro = endereco.Logradouro,
-        //            Numero = endereco.Numero
-        //        }
-        //    };
-
-        //    //Act
-        //    var result = await controller.CadastrarCliente(input, ctx);
-
-        //    //Assert
-        //    Assert.IsType<CreatedResult>(result);
-        //}
-
-        ///// <summary>
-        ///// Dado que:
-        /////    seja solicitado o cadastro de um cliente
-        ///// Eu preciso 
-        /////    validar se os dados informados são válidos e se não existem no banco
-        ///// Para 
-        /////    Informar o motivo do cliente não poder ser cadastrado 
-        ///// </summary>
-        //[Theory]
-        //[InlineData("12345678901", true)]
-        //[InlineData("90459735020", false)]
-        //public async void CadastrarCliente_Erro(string cpf, bool clienteValido)
-        //{
-        //    //Prepare           
-        //    var ctx = CancellationToken.None;
-        //    var controller = CriarUsuarioController(clienteValido);
-
-        //    var endereco = new EnderecoFixture().CriarEndereco();
-
-        //    var input = new ClienteModel
-        //    {
-        //        Aniversario = DateTime.Now.AddYears(-20),
-        //        Cpf = cpf,
-        //        Nome = "Teste",
-        //        Endereco = new ClienteModel.DadosEndereco
-        //        {
-        //            Cep = endereco.Cep,
-        //            Cidade = endereco.Cidade,
-        //            Complemento = endereco.Complemento,
-        //            Estado = endereco.Estado,
-        //            Logradouro = endereco.Logradouro,
-        //            Numero = endereco.Numero
-        //        }
-        //    };
-
-        //    //Act
-        //    var result = await controller.CadastrarCliente(input, ctx);
-
-        //    //Assert
-        //    Assert.IsType<UnprocessableEntityObjectResult>(result);
-        //}
-
-        //#endregion
-
-        //#region Operador
-
-        ///// <summary>
-        ///// Dado que:
-        /////    seja solicitado o cadastro de um operador
-        ///// Eu preciso 
-        /////    validar se os dados informados são válidos e se não existem no banco
-        ///// Para 
-        /////    Armazenar as informações no banco
-        ///// </summary>
-        //[Fact]
-        //public async void CadastrarOperador_Sucesso()
-        //{
-        //    //Prepare           
-        //    var ctx = CancellationToken.None;
-        //    var controller = CriarUsuarioController();
-
-        //    var input = new OperadorModel
-        //    {
-        //        Matricula = "123465",
-        //        Nome = "Teste"
-        //    };
-
-        //    //Act
-        //    var result = await controller.CadastrarOperador(input, ctx);
-
-        //    //Assert
-        //    Assert.IsType<CreatedResult>(result);
-        //}
-
-        ///// <summary>
-        ///// Dado que:
-        /////    seja solicitado o cadastro de um operador
-        ///// Eu preciso 
-        /////    validar se os dados informados são válidos e se não existem no banco
-        ///// Para 
-        /////    Informar o motivo do operador não poder ser cadastrado 
-        ///// </summary>
-        //[Fact]
-        //public async void CadastrarOperador_Erro()
-        //{
-        //    //Prepare           
-        //    var ctx = CancellationToken.None;
-        //    var controller = CriarUsuarioController(true, false);
-
-        //    var input = new OperadorModel
-        //    {
-        //        Matricula = "123465",
-        //        Nome = "Teste"
-        //    };
-
-        //    //Act
-        //    var result = await controller.CadastrarOperador(input, ctx);
-
-        //    //Assert
-        //    Assert.IsType<UnprocessableEntityObjectResult>(result);
-        //}
-
-        //#endregion
-
-        private VeiculosController CriarUsuarioController(bool veiculoValido = true, bool marcaValida = true, bool modeloValido = true, bool agendamentoValido = true)
+        [Fact]
+        public async void CadastrarVeiculo_Sucesso()
         {
-            var veiculoRepository = new VeiculoRepositoryMock().ObterPorId(veiculoValido).Salvar().VerificarSeExiste(!veiculoValido).ListarTodos();
-            var marcaVeiculoRepository = new MarcaVeiculoRepositoryMock().ObterPorCodigo(marcaValida).Salvar().VerificarSeExiste(!marcaValida);
-            var modeloRepository = new ModeloVeiculoRepositoryMock().ObterPorCodigo(modeloValido).Salvar().VerificarSeExiste(!modeloValido);
-            var agendamentoRepository = new AgendamentoRepositoryMock().ObterPorReserva(agendamentoValido).Salvar().VerificarSeExiste(!agendamentoValido);
+            //Prepare           
+            var ctx = CancellationToken.None;
+            var controller = CriarUsuarioController(true, true, true, true, false);
+
+            var input = new VeiculoFixture().CriarVeiculoModel();
+
+            //Act
+            var result = await controller.CadastrarVeiculo(input, ctx);
+
+            //Assert
+            Assert.IsType<CreatedResult>(result);
+        }
+
+        /// <summary>
+        /// Dado que:
+        ///    seja solicitado o cadastro de um veiculo
+        /// Eu preciso 
+        ///    validar se os dados informados são válidos e se não existem no banco
+        /// Para 
+        ///    informar os motivos de não ser possivel cadastrar
+        /// </summary>
+        [Theory]
+        [InlineData(false, true)]
+        public async void CadastrarVeiculo_Erro(bool veiculoValido, bool veiculoExiste)
+        {
+            //Prepare           
+            var ctx = CancellationToken.None;
+            var controller = CriarUsuarioController(veiculoValido, true, true, true, veiculoExiste);
+
+            var input = new VeiculoFixture().CriarVeiculoModel();
+
+            //Act
+            var result = await controller.CadastrarVeiculo(input, ctx);
+
+            //Assert
+            Assert.IsType<UnprocessableEntityObjectResult>(result);
+        }
+
+        #endregion
+
+        #region Marca
+
+        /// <summary>
+        /// Dado que:
+        ///    seja solicitado o cadastro de uma marca
+        /// Eu preciso 
+        ///    validar se os dados informados são válidos e se não existem no banco
+        /// Para 
+        ///    Armazenar as informações no banco
+        /// </summary>
+        [Fact]
+        public async void CadastrarMarca_Sucesso()
+        {
+            //Prepare           
+            var ctx = CancellationToken.None;
+            var controller = CriarUsuarioController(true, true, true, true, true, false);
+
+            var input = new VeiculoFixture().CriarMarcaVeiculoModel();
+
+            //Act
+            var result = await controller.CadastrarMarcaVeiculo(input, ctx);
+
+            //Assert
+            Assert.IsType<CreatedResult>(result);
+        }
+
+        /// <summary>
+        /// Dado que:
+        ///    seja solicitado o cadastro de uma marca
+        /// Eu preciso 
+        ///    validar se os dados informados são válidos e se não existem no banco
+        /// Para 
+        ///    Informar os motivos da marca nao ter sido cadastrada
+        /// </summary>
+        [Fact]
+        public async void CadastrarMarca_Erro()
+        {
+            //Prepare           
+            var ctx = CancellationToken.None;
+            var controller = CriarUsuarioController(true, false);
+
+            var input = new VeiculoFixture().CriarMarcaVeiculoModel();
+
+            //Act
+            var result = await controller.CadastrarMarcaVeiculo(input, ctx);
+
+            //Assert
+            Assert.IsType<UnprocessableEntityObjectResult>(result);
+        }
+
+        #endregion
+
+        #region Modelo
+
+        /// <summary>
+        /// Dado que:
+        ///    seja solicitado o cadastro de um modelo
+        /// Eu preciso 
+        ///    validar se os dados informados são válidos e se não existem no banco
+        /// Para 
+        ///    Armazenar as informações no banco
+        /// </summary>
+        [Fact]
+        public async void CadastrarModelo_Sucesso()
+        {
+            //Prepare           
+            var ctx = CancellationToken.None;
+            var controller = CriarUsuarioController(true, true, true, true, true, true, false);
+
+            var input = new VeiculoFixture().CriarModeloVeiculoModel();
+
+            //Act
+            var result = await controller.CadastrarModeloVeiculo(input, ctx);
+
+            //Assert
+            Assert.IsType<CreatedResult>(result);
+        }
+
+        /// <summary>
+        /// Dado que:
+        ///    seja solicitado o cadastro de um modelo
+        /// Eu preciso 
+        ///    validar se os dados informados são válidos e se não existem no banco
+        /// Para 
+        ///    Informar o motivo do modelo não ter sido cadastrado
+        /// </summary>
+        [Fact]
+        public async void CadastrarModelo_Erro()
+        {
+            //Prepare           
+            var ctx = CancellationToken.None;
+            var controller = CriarUsuarioController(true, true, false);
+
+            var input = new VeiculoFixture().CriarModeloVeiculoModel();
+
+            //Act
+            var result = await controller.CadastrarModeloVeiculo(input, ctx);
+
+            //Assert
+            Assert.IsType<UnprocessableEntityObjectResult>(result);
+        }
+
+        #endregion
+
+        #region Agendamento
+
+        /// <summary>
+        /// Dado que:
+        ///    seja solicitado o agendaemnto de um veiculo
+        /// Eu preciso 
+        ///    validar se os dados informados são válidos e se não existem no banco
+        /// Para 
+        ///    Armazenar as informações no banco
+        /// </summary>
+        [Fact]
+        public async void CadastrarAgendamento_Sucesso()
+        {
+            //Prepare           
+            var ctx = CancellationToken.None;
+            var controller = CriarUsuarioController(true, true, true, true, true, true, true, false);
+
+            var input = new VeiculoFixture().CriarAgendamentoInputModel();
+
+            //Act
+            var result = await controller.AgendarVeiculo(input, ctx);
+
+            //Assert
+            Assert.IsType<CreatedResult>(result);
+        }
+
+        /// <summary>
+        /// Dado que:
+        ///    seja solicitado o agendaemnto de um veiculo
+        /// Eu preciso 
+        ///    validar se os dados informados são válidos e se não existem no banco
+        /// Para 
+        ///    Informar o motivo de nao ser possivel armazenar o agendamento
+        /// </summary>
+        [Theory]
+        [InlineData("", 0, true)]
+        [InlineData("AAABHZ", 10, true)]
+        [InlineData("AAABHZ", -1, true)]
+        [InlineData("AAABHZ", 0, false)]
+        public async void CadastrarAgendamento_Erro(string placa, int dataRetiradaAddAno, bool veiculoValido)
+        {
+            //Prepare           
+            var ctx = CancellationToken.None;
+            var controller = CriarUsuarioController(veiculoValido, true, true, true, true, true, true, true);
+
+            var input = new VeiculoFixture().CriarAgendamentoInputModel();
+            input.Placa = placa;
+
+            if (dataRetiradaAddAno < 0)
+                input.DataRetirada = DateTime.MinValue;
+            else
+                input.DataRetirada = input.DataRetirada.AddYears(dataRetiradaAddAno);
+
+            //Act
+            var result = await controller.AgendarVeiculo(input, ctx);
+
+            //Assert
+            Assert.IsType<UnprocessableEntityObjectResult>(result);
+        }
+
+        #endregion
+
+        private VeiculosController CriarUsuarioController(bool veiculoValido = true, bool marcaValida = true, bool modeloValido = true, bool agendamentoValido = true,
+                                                          bool veiculoExiste = true, bool marcaExiste = true, bool modeloExiste = true, bool agendamentoExiste = true)
+        {
+            var veiculoRepository = new VeiculoRepositoryMock().ObterPorId(veiculoValido).Salvar().VerificarSeExiste(veiculoExiste).ListarTodos();
+            var marcaVeiculoRepository = new MarcaVeiculoRepositoryMock().ObterPorCodigo(marcaValida).Salvar().VerificarSeExiste(marcaExiste);
+            var modeloRepository = new ModeloVeiculoRepositoryMock().ObterPorCodigo(modeloValido).Salvar().VerificarSeExiste(modeloExiste);
+            var agendamentoRepository = new AgendamentoRepositoryMock().ObterPorReserva(agendamentoValido).Salvar().VerificarSeExiste(agendamentoExiste);
             var veiculoApplication = new VeiculoApplication(_mapperFixture.Mapper, veiculoRepository.Object, marcaVeiculoRepository.Object, modeloRepository.Object, agendamentoRepository.Object);
 
             return new VeiculosController(_mapperFixture.Mapper, veiculoApplication);
--- src/Teste.El.Backend.Tests/Fixtures/VeiculoFixture.cs
+++ src/Teste.El.Backend.Tests/Fixtures/VeiculoFixture.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Teste.El.Backend.Application.Models;
 using Teste.El.Backend.Domain.Entities;
 using Teste.El.Backend.Domain.Enums;
 
@@ -25,6 +26,49 @@ namespace Teste.El.Backend.Tests.Fixtures
                 Placa = "AAA1234",
                 ValorHora = 10
             };  
-        }       
+        }
+
+        public VeiculoModel CriarVeiculoModel()
+        {
+            return new VeiculoModel
+            {
+                Ano = 2021,
+                Categoria = ECategoriaVeiculo.Basico,
+                Marca = "1234",
+                Modelo = "1234",
+                Combustivel = ETipoCombustivel.Gasolina,
+                LimitePortaMalas = 80,
+                Placa = "AAA1234",
+                ValorHora = 10
+            };
+        }
+
+        public MarcaVeiculoModel CriarMarcaVeiculoModel()
+        {
+            return new MarcaVeiculoModel
+            {
+                Codigo = "1234",
+                Descricao = "Fiat"
+            };
+        }
+
+        public ModeloVeiculoModel CriarModeloVeiculoModel()
+        {
+            return new ModeloVeiculoModel
+            {
+                Codigo = "1234",
+                Descricao = "Palio 1.0"
+            };
+        }
+
+        public AgendamentoInputModel CriarAgendamentoInputModel()
+        {
+            return new AgendamentoInputModel
+            { 
+                DataDevolucao = DateTime.Now.AddDays(3),
+                DataRetirada = DateTime.Now,
+                Placa = "AAA1234"
+            };
+        }
     }
 }
